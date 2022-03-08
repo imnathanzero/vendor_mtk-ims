2ba3a6955e1598df7db74ecd99f9caba4f415b11
AIV-97 <AIV-97@users.noreply.github.com>
2022-03-08 19:22:29 +0700

    RMX1941: Update Ims Blobs from Srock C55
    Signed-off-by: AIV-97 <AIV-97@users.noreply.github.com>

--- ims.mk
+++ ims.mk
@@ -1,16 +1,19 @@
-PRODUCT_PACKAGES += \
-	ImsServiceBase
+# Mtk IMS
+PRODUCT_COPY_FILES += \
+    vendor/mtk-ims/proprietary/framework/mediatek-ims-extension-plugin.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-extension-plugin.jar \
+    vendor/mtk-ims/proprietary/framework/mediatek-ims-legacy.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-legacy.jar \
+    vendor/mtk-ims/proprietary/framework/mediatek-ims-wwop-common.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-wwop-common.jar \
 
 PRODUCT_PACKAGES += \
+	ImsServiceBase \
+    ImsService \
     mediatek-common \
     mediatek-framework \
     mediatek-ims-base \
     mediatek-ims-common \
-    mediatek-ims-legacy \
-    mediatek-ims-extension-plugin \
     mediatek-telecom-common \
     mediatek-telephony-base \
-    mediatek-telephony-common
+    mediatek-telephony-common \
 
 PRODUCT_BOOT_JARS += \
     mediatek-common \
