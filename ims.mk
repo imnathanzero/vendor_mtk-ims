906cadf31c60e53101118aa974a7c71daef2da5d
AIV-97 <fraschze97@nusantararom.org>
2022-11-02 10:33:50 +0700

    fix compilation

--- ims.mk
+++ ims.mk
@@ -1,9 +1,4 @@
 # Mtk IMS
-PRODUCT_COPY_FILES += \
-    vendor/mtk-ims/proprietary/framework/mediatek-ims-extension-plugin.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-extension-plugin.jar \
-    vendor/mtk-ims/proprietary/framework/mediatek-ims-legacy.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-legacy.jar \
-    vendor/mtk-ims/proprietary/framework/mediatek-ims-wwop-common.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/mediatek-ims-wwop-common.jar \
-
 PRODUCT_PACKAGES += \
 	ImsServiceBase \
     ImsService \
